4b7bbc2ea1168c1cb9fc7ae6290b47223dcbe041
Caijie <caijie96@gmail.com>
2021-01-25 00:48:15 +0800

    Init

---
+++ src/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import {Container} from "@material-ui/core";
+
+const localizer = momentLocalizer(moment);
+export default function Home() {
+  return (
+    <Container>
+      <p>
+        Please select a date and key in your desired time. Make sure it doesn't clash!
+      </p>
+      <Calendar
+        localizer={localizer}
+        events={[]}
+        startAccessor="start"
+        endAccessor="end"
+        style={{height: 800}}
+        selectable={true}
+        views={['week']}
+      />
+    </Container>
+  )
+}
