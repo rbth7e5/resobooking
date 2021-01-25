f983e686ed1fde4db8137cd26663774fb73ebd86
Caijie <caijie96@gmail.com>
2021-01-26 04:43:26 +0800

    Add auto email

--- src/api.js
+++ src/api.js
@@ -1,4 +1,7 @@
+import emailjs from "emailjs-com";
+import moment from "moment";
 const RESO_CAL_ID = "c_5tokio7mfc7jmv4qivgv5fdlr0@group.calendar.google.com";
+emailjs.init("user_K1PTj6uuUPamMKv16fNbp");
 export async function approveEvent(accessToken, event) {
   event.start.dateTime = event.start.dateTime.toDate();
   event.end.dateTime = event.end.dateTime.toDate();
@@ -14,10 +17,15 @@ export async function approveEvent(accessToken, event) {
     }
   );
   if (response.ok) {
-    sendEmail(event.email);
+    emailjs.send("service_pjlhoib", "template_ng0dn7k", {
+      from_name: "NUS Resonance Practice Booking System",
+      to_name: event.name,
+      message: `Your booking request has been approved for ${
+        event.summary
+      } from ${moment(event.start.dateTime).format(
+        "Do MMM YYYY HH:mm"
+      )} to ${moment(event.end.dateTime).format("Do MMM YYYY HH:mm")}`,
+      to_email: "caijie96@gmail.com",
+    });
   }
-}
-
-function sendEmail(address) {
-  window.location = `mailto:${address}`;
 }
